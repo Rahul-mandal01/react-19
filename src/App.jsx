dfa2bfb45eb515a0840fcb1af65f18d76be55dd4
rahul <rahulra16809999@gmail.com>
2025-02-24 18:25:33 +0530

    Initial implementation of App component with conditional rendering
    - Added useState hook to manage count state.
    - Implemented a button to increment the count.
    - Added conditional rendering to display different messages based on the count value.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
-import  Counter  from "./Counter";
+import User from "./User";
 function App() {
-  const [fruit, setFruit] = useState("Apple");
-  const handleFruit = () => {
-    setFruit("Banana");
-  }
+    // const [display, setDisplay] = useState(true);
+    const [count, setCount] = useState(0);
     return(
       <>
-        <h1>State in React Js </h1>
-        <h2>{fruit}</h2>
-        <button onClick={handleFruit}> Change Fruit Name </button>
-        <Counter/>
+        {/* <h1>Toggle in React Js </h1> */}
+        {/* <button onClick={() =>setDisplay(!display)}>Toggle</button> */}
+        {/* {
+          display ? <h2>Abhi Dikh Gya, ab phir click karoge to nahi dikhunga</h2> : null
+        } */}
+
+        {/* {
+          display ? <User /> : null
+        } */}
+
+
+        {/* with Multiple Condition */}
+
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count+1)}>Counter</button>
+        {
+          count == 0 ? <h2>Condition 0</h2>
+          : count == 1 ? <h2>Condition 1</h2>
+          : count == 2 ? <h2>Condition 2</h2>
+          : count == 3 ? <h2>Condition 3</h2>
+          : count == 4 ? <h2>Condition 4</h2>
+          : count == 5? <h2>Condition 5</h2>
+          :<h2>Other Condition</h2>
+        }
       </>
     )
 
