0b538778e71dab9a27576c9557e96a5abe90d687
rahul <rahulra16809999@gmail.com>
2025-02-25 19:51:43 +0530

    Added Student component
    - Created Student component to display student name.
    - Implemented props to pass and display student name.

---
+++ src/Student.jsx
@@ -0,0 +1,11 @@
+function Student({name}){
+    return(
+        <>
+            <hr/>
+            <h1>Student Name: {name}</h1>
+            <hr/>
+        </>
+    )
+}
+
+export default Student;
