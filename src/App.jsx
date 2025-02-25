cc76f07e2f1ed4f42660021722cd2152ef39f486
rahul <rahulra16809999@gmail.com>
2025-02-25 19:50:44 +0530

    Implemented App component with user and college data
    - Added user objects with name, age, and email properties.
    - Included college names array.
    - Implemented state management for student name.
    - Rendered User and College components with appropriate props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { useState } from "react";
 import User from "./User";
+import College from "./College";
+import Student from "./Student";
 function App() {
-    // const [display, setDisplay] = useState(true);
-    const [count, setCount] = useState(0);
+  // let userName = "Rahul";
+  // let age=23;
+  // let email="rahul@test.com"
+
+  let userObject1={
+    name:"Rahul",
+    age:23,
+    email:"rahul@test.com"
+  }
+
+  let userObject2={
+    name:"Anil",
+    age:25,
+    email:"anil@test.com"
+  }
+
+  let userObject3={
+    name:"Steve",
+    age:30,
+    email:"steve@test.com"
+  }
+
+  let collegeNames = ['IIT', 'NIT', 'NSEC', 'IIEST', 'JU']
+  const[student, setStudent] = useState()
     return(
       <>
-        {/* <h1>Toggle in React Js </h1> */}
-        {/* <button onClick={() =>setDisplay(!display)}>Toggle</button> */}
-        {/* {
-          display ? <h2>Abhi Dikh Gya, ab phir click karoge to nahi dikhunga</h2> : null
-        } */}
-
-        {/* {
-          display ? <User /> : null
-        } */}
-
-
-        {/* with Multiple Condition */}
-
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count+1)}>Counter</button>
-        {
-          count == 0 ? <h2>Condition 0</h2>
-          : count == 1 ? <h2>Condition 1</h2>
-          : count == 2 ? <h2>Condition 2</h2>
-          : count == 3 ? <h2>Condition 3</h2>
-          : count == 4 ? <h2>Condition 4</h2>
-          : count == 5? <h2>Condition 5</h2>
-          :<h2>Other Condition</h2>
-        }
+       <h1>Props in React js</h1>
+       {student && <Student name={student}/>}
+       <button onClick={()=>setStudent("Rahul")}>Update Student Name</button>
+       <College names={collegeNames[2]}/>
+       {/* <User name="Rahul" age="22" email="test@gmail.com"/> */}
+        {/* <User name={userName} age={age} email={email} /> */}
+       <User user={userObject1}/>
+       <User user={userObject2}/>
+       <User user={userObject3}/>
       </>
     )
 
