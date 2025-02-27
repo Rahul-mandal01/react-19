8fee4ce4177b99afd5a3aaedb6c0c86ec7a9c106
rahul <rahulra16809999@gmail.com>
2025-02-27 22:12:15 +0530

    Delete Input field code

--- src/App.jsx
+++ src/App.jsx
@@ -11,39 +11,8 @@ function App() {
 
   return (
     <>
-      <h1>Get Input Field Value</h1>
+      <h1>Controlled Component</h1>
 
-      <input
-        type="text"
-        value={user}
-        onChange={(event) => setUser(event.target.value)}
-        placeholder="Enter User Name"
-        onBlur={clear} // Call clear when input loses focus
-      />
-      <h1>{user}</h1>
-      <button onClick={() => setUser("")}>Clear Value</button>
-      <hr />
-
-      <input
-        type="text"
-        value={place}
-        onChange={(event) => setPlace(event.target.value)}
-        placeholder="Enter User Place"
-        onBlur={clear}
-      />
-      <h1>{place}</h1>
-      <button onClick={() => setPlace("")}>Clear Value</button>
-      <hr />
-
-      <input
-        type="number"
-        value={age}
-        onChange={(event) => setAge(event.target.value)}
-        placeholder="Enter User Age"
-        onBlur={clear}
-      />
-      <h1>{age}</h1>
-      <button onClick={() => setAge("")}>Clear Value</button>
     </>
   );
 }
