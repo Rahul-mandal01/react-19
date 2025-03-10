4037fdfcc11f451bd2425b107bf7183a486d3f23
rahul <rahulra16809999@gmail.com>
2025-03-10 23:19:18 +0530

    Clear unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-
-function clear(event) {
-  console.log(event.target.value);
-}
-
 function App() {
 
   return (
     <>
-      {/* <h1>Controlled Component</h1> */}
+      <h1>Controlled Component</h1>
     </>
   );
 }
