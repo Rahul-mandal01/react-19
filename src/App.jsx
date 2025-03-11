0cd82b2c7d718964b9d392c341419ae3f616c20e
rahul <rahulra16809999@gmail.com>
2025-03-11 20:42:03 +0530

    Add controlled components for form inputs and implement reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
 
-function clear(event) {
-  console.log(event.target.value);
-}
-
 function App() {
-  const [user, setUser] = useState("");
-  const [place, setPlace] = useState("");
-  const [age, setAge] = useState("");
-
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
   return (
     <>
       <h1>Controlled Component</h1>
+      <form action="" method="get" >
+        <input  type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter Name" />
+        <br/> <br/>
+        <input  type="password" value={password} onChange={(event) => setPassword(event.target.value)} placeholder="Enter Password" />
+        <br/> <br/>
+        <input  type="text" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Enter Email" />
+        <br/> <br/>
 
+        <button>Submit</button>
+
+        <button onClick={() => {setEmail(''); setName(''); setPassword('')}} >Clear</button>
+
+        <h2>{name}</h2>
+        <h2>{password}</h2>
+        <h2>{email}</h2>
+      </form>
     </>
   );
 }
