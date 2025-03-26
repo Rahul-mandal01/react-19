d756a81c4b6b0b666b53721f603705877c2210b6
rahul <rahulra16809999@gmail.com>
2025-03-26 22:07:56 +0530

    Removed dropdown codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
 import { useState } from "react";
 
 function App() {
-  const [gender, setGender] = useState("female");
-  const [city, setCity] = useState("Kolkata");
   return (
     <>
-      <h1>Handle Radio and Dropdown</h1>
-      <h4>Select Gender</h4>
-      <input
-        type="radio"
-        name="gender"
-        id="male"
-        value={"male"}
-        onChange={(event) => setGender(event.target.value)}
-        checked={gender == "male"}
-      />
-      <label htmlFor="male">Male</label>
-      <input
-        type="radio"
-        name="gender"
-        id="female"
-        value={"female"}
-        onChange={(event) => setGender(event.target.value)}
-        checked={gender == "female"}
-      />
-      <label htmlFor="female">Female</label>
-
-      <h2>Selected Gender: {gender}</h2>
-
-      <br />
-y
-
-      <h4>Select City</h4>
-
-      <select
-        defaultValue={"Kolkata"}
-        onChange={(event) => setCity(event.target.value)}
-      >
-        <option value="Noida">Noida</option>
-        <option value="Kolkata">Kolkata</option>
-        <option value="Delhi">Delhi</option>
-      </select>
-
-      <h2>Selected City : {city}</h2>
+      
     </>
   );
 }
