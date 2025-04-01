3a06dcfbf9086f47afb24ee11d2fdbbad2c43e6a
rahul <rahulra16809999@gmail.com>
2025-04-01 20:57:27 +0530

    Added table rendering with user data using the map function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import { useState } from "react";
 
 function App() {
+  const userNames = ['rahul', 'sam', 'john', 'rohan'];
+
+  const userData = [
+    {
+      name: 'rahul',
+      age: 28,
+      city: 'delhi',
+      id: 1
+    },
+    {
+      name:'sam',
+      age: 30,
+      city: 'mumbai',
+      id: 2
+    },
+    {
+      name: 'john',
+      age: 26,
+      city: 'chennai',
+      id: 3
+    },
+    {
+      name : 'rohan',
+      age: 35,
+      city: 'bangalore',
+      id: 4
+    }
+
+  ]
   return (
     <>
-      
+      <h1>Loop in JSX with Map Function</h1>
+      <table border="1">
+        <thead>
+          <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>City</td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+                <td>{user.city}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </>
   );
 }
