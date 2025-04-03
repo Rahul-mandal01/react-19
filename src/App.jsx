5288d8db9883dc5b3945b5c69a693bac4056749c
rahul <rahulra16809999@gmail.com>
2025-04-03 16:12:28 +0530

    Added useEffect hooks for initial function calls and state management
    - Implemented useEffect to call counterFunction on initial render.
    - Added another useEffect to call callOnce function on initial render.
    - Managed counter and data states with buttons to update their values.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState(0);
+
+  useEffect (()=>{
+    counterFunction()
+  },[])
+
+  useEffect(()=>{
+    callOnce();
+  },[])
+
+  function callOnce(){
+    console.log("callOnce function called");
+  }
+
+  function counterFunction (){
+    console.log("Counter function called");
+  }
+
   return (
     <>
-      
+      <h1>useEffect Hook</h1>
+      <button onClick={() =>setCounter(counter+1)}>Counter : {counter}</button>
+      <button onClick={()=>setData(data+1)} >Data : {data}</button>
     </>
   );
 }
