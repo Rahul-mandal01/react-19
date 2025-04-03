3713fb3badeb2081a35c4d750ff24f5706e26da1
rahul <rahulra16809999@gmail.com>
2025-04-03 18:43:58 +0530

    Enhanced App component with dynamic theming and grid toggle
    - Added functionality to dynamically update card background and text color.
    - Implemented a grid toggle feature to switch between grid and block layouts.
    - Rendered user cards dynamically using the User array.
    - Improved inline styling for better visual consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,38 @@
+import { useState } from "react";
+import User from "./User.js";
 
 function App() {
-  
-  const cardStyle = {
-    border : "1px solid #cccccc3b",
-    width : "200px",
-    boxShadow : "1px 2px 3px 0px #cccccc57",
-    margin : "10px"
+  const [cardStyle, setCardStyle] = useState({
+    border: "1px solid #cccccc3b",
+    width: "200px",
+    boxShadow: "1px 2px 3px 0px #cccccc57",
+    margin: "10px",
+  });
+
+  const [textColor, setTextColor] = useState("black");
+  const [grid, setGrid] = useState(true);
+
+  const updateTheme = (bgColor, textColor) => {
+    setCardStyle({...cardStyle, backgroundColor: bgColor});
+    setTextColor(textColor);
   }
 
   return (
     <>
-      <h1 style={{color:"red"}}>Inline Style in React Js</h1>
-      <div style={{display:"flex", flexWrap:"wrap"}}>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
+      <h1 style={{ color: "red" }}>Dynamic and Conditional Inline Style in React Js</h1>
+      <button onClick={()=> updateTheme('#ccc' , 'red')}>Gray Theme</button>
+      <button onClick={()=> updateTheme('white' , 'black')}>Default Theme</button>
+      <button onClick={()=> setGrid(!grid)}>Toggle Grid</button>
+      <div style={{ display: grid ? 'flex' :'block', flexWrap: "wrap" }}>
+        {User.map((user, index) => (
+          <div style={cardStyle} key={index}>
+            <img style={{ width: "202px" }} src={user.image} />
+            <div style={{ padding: "5px", color: textColor }}>
+              <h4>{user.name}</h4>
+              <p>{user.designation}</p>
+            </div>
           </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
-        <div style={cardStyle}>
-          <img style={{width:"202px"}} src="https://www.w3schools.com/howto/img_avatar.png" />
-          <div style={{padding: "5px"}}>
-            <h4>Rahul Kumar</h4>
-            <p>Software Engineer</p>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
