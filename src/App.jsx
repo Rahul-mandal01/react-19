1948898678991f5530144ac5362192b51795562a
rahul <rahulra16809999@gmail.com>
2025-04-03 16:31:43 +0530

    Initialized App component with state and Counter integration
    - Added useState hooks to manage count and data states.
    - Integrated Counter component to display count and data values.
    - Implemented buttons to increment count and data states.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
+import Counter from "./Counter";
+
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
   return (
     <>
-
+      <Counter count={count} data={data} />
+      <button onClick={()=>setCount(count+1)}>Counter</button>
+      <button onClick={()=>setData(data+1)}>Data</button>
     </>
   );
 }
