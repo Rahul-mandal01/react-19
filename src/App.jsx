d788216bcc67a8c118e473e904e6efe0dcf9a1b9
rahul <rahulra16809999@gmail.com>
2025-04-03 13:36:09 +0530

    Implemented nested looping to render college data dynamically
    - Added a map function to iterate over collegeData and render College components.
    - Styled the layout using flexbox for better alignment.
    - Displayed college details dynamically using props.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,82 @@
-
+import College from "./College";
 
 function App() {
+  const collegeData = [
+    {
+      name: "NSEC Kolkata",
+      location: "Kolkata",
+      website: "www.nsec.ac.in",
+      courses: [
+        {
+          name: "B.Tech",
+          duration: "4 years",
+        },
+        {
+          name: "M.Tech",
+          duration: "2 years",
+        },
+        {
+          name: "MBA",
+          duration: "2 years",
+        },
+      ],
+    },
+
+    {
+      name: "IEM Kolkata",
+      location: "Kolkata",
+      website: "www.iem.edu.in",
+      courses: [
+        {
+          name: "B.Tech",
+          duration: "4 years",
+        },
+        {
+          name: "M.Tech",
+          duration: "2 years",
+        },
+        {
+          name: "MBA",
+          duration: "2 years",
+        },
+      ],
+    },
+    {
+      name: "NIT Kolkata",
+      location: "Kolkata",
+      website: "www.jis.edu.in",
+      courses: [
+        {
+          name: "B.Tech",
+          duration: "4 years",
+        },
+        {
+          name: "M.Tech",
+          duration: "2 years",
+        },
+        {
+          name: "MBA",
+          duration: "2 years",
+        },
+      ],
+    },
+  ];
   return (
     <>
+      <h1>Nested Looping in React js</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {collegeData.map((college, index) => (
+          <div key={index}>
+            <College college={college} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
