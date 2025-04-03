6144938ba11aeed9eed22fd0a0e6845f39eb5e96
rahul <rahulra16809999@gmail.com>
2025-04-03 12:38:10 +0530

    Added color selection functionality to App component
    - Implemented a dropdown to select colors dynamically.
    - Used useState to manage the selected color state.
    - Passed the selected color as a prop to the Clock component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Clock from "./Clock";
+
 function App() {
+  const [color, setColor] = useState("white");
   return (
     <>
-
+      <h1>Digital Clock in React js</h1>
+      <select onChange={(e) => setColor(e.target.value)}>
+        <option value={"yellow"}>Yellow</option>
+        <option value={"red"}>Red</option>
+        <option value={"orange"}>Orange</option>
+        <option value={"green"}>Green</option>
+      </select>
+      <Clock color={color} />
     </>
   );
 }
