c2f91063920bf6b89c3cb5ea9e0c8fcf4a1f56df
rahul <rahulra16809999@gmail.com>
2025-04-05 23:13:41 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,15 @@ import { useState } from "react";
 import User from "./User.js";
 
 function App() {
-  const [cardStyle, setCardStyle] = useState({
-    border: "1px solid #cccccc3b",
-    width: "200px",
-    boxShadow: "1px 2px 3px 0px #cccccc57",
-    margin: "10px",
-  });
-
-  const [textColor, setTextColor] = useState("black");
-  const [grid, setGrid] = useState(true);
-
-  const updateTheme = (bgColor, textColor) => {
-    setCardStyle({...cardStyle, backgroundColor: bgColor});
-    setTextColor(textColor);
-  }
 
   return (
     <>
-      <h1 style={{ color: "red" }}>Dynamic and Conditional Inline Style in React Js</h1>
-      <button onClick={()=> updateTheme('#ccc' , 'red')}>Gray Theme</button>
-      <button onClick={()=> updateTheme('white' , 'black')}>Default Theme</button>
-      <button onClick={()=> setGrid(!grid)}>Toggle Grid</button>
-      <div style={{ display: grid ? 'flex' :'block', flexWrap: "wrap" }}>
+      <h1>External Style in React Js</h1>
+      <div>
         {User.map((user, index) => (
-          <div style={cardStyle} key={index}>
-            <img style={{ width: "202px" }} src={user.image} />
-            <div style={{ padding: "5px", color: textColor }}>
+          <div key={index}>
+            <img src={user.image} />
+            <div>
               <h4>{user.name}</h4>
               <p>{user.designation}</p>
             </div>
