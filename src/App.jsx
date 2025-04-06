b97df18a8835d479dbb50d9280db6688393dd24d
rahul <rahulra16809999@gmail.com>
2025-04-06 19:15:33 +0530

    Refactor App.jsx to use the UserProfile component for rendering user cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import './css/style.css';
-import User from "./User.js";
+import UserProfile from './UserProfile';
 
 function App() {
 
   return (
     <>
       <h1 className='heading' >External Style in React Js</h1>
-      <div className='container'>
-        {User.map((user, index) => (
-          <div key={index} className='user-card'>
-            <img src={user.image} className='img-style' />
-            <div className='text-wrap'>
-              <h4>{user.name}</h4>
-              <p>{user.designation}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <UserProfile />
     </>
   );
 }
