2e61b2359e59ebe0e15a667ca7b324623d033a09
rahul <rahulra16809999@gmail.com>
2025-04-07 20:27:53 +0530

    Integrate React-Bootstrap components for Navbar, Alerts, and Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,68 @@
-import styled from 'styled-components';
-
-// const Heading =  styled.h1`
-//   color : red;
-//   border : 1px solid green;
-//   background-color : blue;
-//   padding : 10px;
-//   text-align : center;
-//   border-radius : 10px;
-// `
-
-const Heading = styled.h1({
-  color : 'red',
-  border : '1px solid green',
-  backgroundColor : 'blue',
-  padding : '10px',
-  textAlign : 'center',
-  borderRadius : '10px',
-})
-
-const StyleBtn = styled.button`
-  background-color : blue;
-  color : white;
-  padding : 10px;
-  border : none;
-  border-radius : 5px;
-  cursor : pointer;
-  width : 200px;
-  height : 50px;
-  margin-right : 50px;
-`
+import { Alert, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
-
   return (
     <>
-      <h1> Styled Component with React Js </h1>
-      <Heading> Hello World </Heading>
-      <Heading> Hello World </Heading>
-      <Heading> Hello World </Heading>
-      <StyleBtn> Click Me </StyleBtn>
-      <StyleBtn> Click Me </StyleBtn>
-      <StyleBtn> Click Me </StyleBtn>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <h1>Bootstrap</h1>
+      <Alert variant="success" className="mt-3">
+        This is a success alert—check it out!
+      </Alert>
+
+      <Alert variant="danger" className="mt-3">
+        This is a danger alert—check it out!
+      </Alert>
+
+      <Button onClick={() => alert("Hii")} variant="primary">
+        Primary
+      </Button>
+      <Button variant="secondary" className="p-1">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+
+      <Button onClick={() => alert("hello variant")} variant="outline-primary">
+        Primary
+      </Button>
+      <Button variant="outline-secondary">Secondary</Button>
+      <Button variant="outline-success">Success</Button>
+      <Button variant="outline-warning">Warning</Button>
+      <Button variant="outline-danger">Danger</Button>
+      <Button variant="outline-info">Info</Button>
+      <Button variant="outline-light">Light</Button>
+      <Button variant="outline-dark">Dark</Button>
     </>
   );
 }
