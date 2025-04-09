e06ff1fedf8b15ccfaa964bf40f1a5e07d8016d5
rahul <rahulra16809999@gmail.com>
2025-04-09 23:16:25 +0530

    Implement useRef for input field manipulation and toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useRef } from "react";
+
 function App() {
+
+  const inputRef = useRef(null);
+  const inputHandler = () => {
+    console.log(inputRef);
+    inputRef.current.focus();
+    inputRef.current.style.color = 'red'
+    inputRef.current.placeholder = 'Hello World'
+    inputRef.current.value = 'Hello world'
+    console.log(inputRef.current.value);
+  }
+
+  const toggleHandler = () => {
+    if( inputRef.current.style.display != 'none'){
+      inputRef.current.style.display = 'none'
+    }else{
+      inputRef.current.style.display = 'inline'
+    }
+  }
   return (
     <>
-
+      <h1>useRef</h1>
+      <button onClick={toggleHandler} >Toggle</button>
+      <input ref={inputRef} type="text" placeholder="Enter user name" />
+      <button onClick={inputHandler} >Focus on Input Field</button>
     </>
   );
 }
