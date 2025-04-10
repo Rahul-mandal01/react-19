8c0db1eecbaffd01623524ae6fbf8f18c2ec6169
rahul <rahulra16809999@gmail.com>
2025-04-10 20:56:04 +0530

    Enable parent-to-child communication by passing and invoking functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,22 @@
-import { useRef } from "react";
+import User from "./User";
 
 function App() {
 
-  const userRef = useRef();
-  const passwordRef = useRef();
-
-  const handleForm = (event) => {
-    event.preventDefault();
-
-    const user = document.querySelector("#user").value;
-    const password = document.querySelector("#password").value;
-
-    console.log(user, password); 
+  const displayName = (name) => {
+    alert(`Hello ${name}`);
   }
 
-  const handleSubmitRef = (event) => {
-    event.preventDefault();
-
-    const user = userRef.current.value;
-    const password = passwordRef.current.value;
-
-    console.log(user, password);
-    
+  const getUser = () => {
+    alert('Get User Function Called');
   }
 
   return (
     <>
-      <h1>Uncontrolled Component</h1>
-      <form action="" method="post" onSubmit={handleForm} >
-        <input type="text" id="user" placeholder="Enter user name" />
-        <br /> <br/>
-        <input type="password" id="password" placeholder="Enter user password" />
-        <br/> <br/>
-
-        <button>
-          Submit
-        </button>
-      </form>
-
-      <hr/>
-
-      <form onSubmit={handleSubmitRef} method="post" action="">
-        <h1>Uncontrolled Component with useRef</h1>
-        <input type="text" ref={userRef} id="userRef" placeholder="Enter user name"  />
-        <br/> <br/>
-        <input type="password" ref={passwordRef} id="passwordRef" placeholder="Enter user password"  />
-        <br/> <br/>
-
-        <button>
-          Submit with Ref
-        </button>
-      </form>
+     <h1>Call Parent component function from child component</h1>
+     <User displayName={displayName} name="Rahul" getUser={getUser} />
+     <User displayName={displayName} name="Alice" getUser={getUser} />
+     <User displayName={displayName} name="Anil" getUser={getUser} />
+     <User displayName={displayName} name="Sidhu" getUser={getUser} />
     </>
   );
 }
