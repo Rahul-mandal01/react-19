e3208f41a11c74983b56621f260f4172b7f22e0d
rahul <rahulra16809999@gmail.com>
2025-04-11 16:37:12 +0530

    Implement forwardRef to manage child input field from parent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { useRef } from "react";
+import UserInput from "./UserInput";
 
 function App() {
+
+  const inputRef = useRef(null);
+
+  const updateInput = () => {
+        inputRef.current.value = "Updated Value";
+        inputRef.current.focus();
+        inputRef.current.style.backgroundColor = "yellow";
+        inputRef.current.style.color = "red";
+  }
   return (
     <>
-     
+      <h1>forward Ref</h1>
+     <UserInput ref={inputRef} />
+     <button onClick={updateInput} >Update Input Field</button>
     </>
   );
 }
