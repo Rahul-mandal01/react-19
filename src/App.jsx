ed1eed661d1d402ca68ae1b63b1ebf2ba3152117
rahul <rahulra16809999@gmail.com>
2025-04-11 16:21:46 +0530

    Removed props code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import User from "./User";
 
 function App() {
-
-  const displayName = (name) => {
-    alert(`Hello ${name}`);
-  }
-
-  const getUser = () => {
-    alert('Get User Function Called');
-  }
-
   return (
     <>
-     <h1>Call Parent component function from child component</h1>
-     <User displayName={displayName} name="Rahul" getUser={getUser} />
-     <User displayName={displayName} name="Alice" getUser={getUser} />
-     <User displayName={displayName} name="Anil" getUser={getUser} />
-     <User displayName={displayName} name="Sidhu" getUser={getUser} />
+     
     </>
   );
 }
