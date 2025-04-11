48b408985502bcc75d1e2639898b9aec5903c919
rahul <rahulra16809999@gmail.com>
2025-04-11 17:42:32 +0530

    use forwardRef for improved parent-child interaction

--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -14,13 +14,14 @@
 
 
 // REACT 19 VERSION
+import {forwardRef} from "react";
 
-const UserInput = (props) => {
+const UserInput = forwardRef( function UserInput (props, ref){
     return(
         <>
-            <input type="text" ref={props.ref} />
+            <input type="text" ref={ref} />
         </>
     )
-}
+})
 
 export default UserInput;
